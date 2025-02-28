827d03685b93b2326af83e2b37583c89ab04728a
soyxbshxikh <you@example.com>
2025-02-28 10:15:11 +0530

    new

--- Basic/Calculator.py
+++ Basic/Calculator.py
@@ -1,21 +1,19 @@
-def calculator(a, b, operation):
-    match operation:
-        case "+":
-            return a + b
-        case "-":
-            return a - b
-        case "*":
-            return a * b
-        case "/":
-            return a / b
-        case _:
-            return "Invalid operation"
-
 # User input
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 operation = input("Enter operation (+, -, *, /): ")
 
 # Calculate
-result = calculator(a, b, operation)
+match operation:
+    case "+":
+        result = a + b
+    case "-":
+        result = a - b
+    case "*":
+        result = a * b
+    case "/":
+        result = a / b
+    case _:
+        result = "Invalid operation"
+
 print(f"The result is {result}")
